3c9a746e5795e284bf3a8a3a78b2b0069ba9eba6
mohammadmizan <takbir.jcd@gmail.com>
2022-05-30 18:57:44 +0600

    for result I hope its work

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,24 +5,7 @@ export const actionBtn = document.querySelector('.try');
 actionBtn.addEventListener('click', () => {
     imgContent.classList.add('loadingContent');
     imgContent.innerHTML = `
-     <iframe src="./index2.html" style="width: 100%; height: 100%;"></iframe>`
-
-    //     < img
-    // src = "/assets/svg/Graphics-Check-big.svg"
-    // alt = "Graphics-Check-big.svg"
-    // className = "loadImg" >
-    //     < div
-    // className = "loadIcons position-relative" >
-    //     < div
-    // className = "loadIcon position-absolute i-1" >
-    //     < img
-    // src = "/assets/svg/Button%20Camera.svg"
-    // alt = "Button%20Camera.svg" >
-    //     < /div>
-    // <div className="loadIcon i-2 position-absolute">
-    //     <img src="/assets/svg/circle.svg" alt="circle.svg">
-    // </div>
-
+     <iframe src="./index2.html" style="width: 100%; height: 100%;"></iframe>`;
 })
 
 
