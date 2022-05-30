7d9879aea4a92542d091e6b76b71d3bae3cd2be5
mohammadmizan <takbir.jcd@gmail.com>
2022-05-30 16:20:51 +0600

    for result I hope its work

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,7 @@ actionBtn.addEventListener('click', (e) => {
     e.preventDefault()
     imgContent.classList.add('loadingContent');
     imgContent.innerHTML = `
-     <iframe src="./index2.html" style="width: 100%; height: 100%;"></iframe>
-    <div className = "loadIcons position-relative" >
-        <divclassName = "loadIcon position-absolute i-1" >
-        <img src = "/assets/svg/Button%20Camera.svg" alt = "Button%20Camera.svg" >
-       </div>
-    <div className="loadIcon i-2 position-absolute">
-        <img src="/assets/svg/circle.svg" alt="circle.svg">
-    </div>
-    
-    `
+     <iframe src="./index2.html" style="width: 100%; height: 100%;"></iframe>`
 
     //     < img
     // src = "/assets/svg/Graphics-Check-big.svg"
@@ -37,82 +28,6 @@ actionBtn.addEventListener('click', (e) => {
     // camera button click event
     cameraBtn.addEventListener('click', () => {
         //
-        imgContent.classList.remove('loadingContent');
-        imgContent.innerHTML = `
-        <video id="video"></video>
-        <canvas id="canvas"></canvas>
-        `
-
-        const videoElement = document.getElementById('video');
-        const canvasElement = document.getElementById('canvas');
-        const canvasCtx = canvasElement.getContext('2d');
-
-
-        function onResults(results) {
-            canvasCtx.save();
-            canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-            canvasCtx.drawImage(
-                results.image, 0, 0, canvasElement.width, canvasElement.height);
-
-
-            // console.log(`right iris ${FACEMESH_RIGHT_IRIS}`)
-            // console.log(`left iris ${FACEMESH_LEFT_IRIS}`)
-            console.log(typeof FACEMESH_RIGHT_IRIS)
-            for (let i in FACEMESH_RIGHT_IRIS) {
-                console.log(FACEMESH_RIGHT_IRIS[i])
-            }
-            // for separate face detection landmarks
-            results.multiFaceLandmarks.forEach(function (landmarks) {
-
-
-                console.log(`right x ${landmarks[469].x}`)
-                console.log(`right y ${landmarks[469].y}`)
-                console.log(`right z ${landmarks[469].z}`)
-                console.log(`left x ${landmarks[474].x}`)
-                console.log(`left y ${landmarks[474].y}`)
-                console.log(`left z ${landmarks[474].z}`)
-
-
-            })
-
-            // for single face detection landmarks
-
-
-            if (results.multiFaceLandmarks) {
-                for (const landmarks of results.multiFaceLandmarks) {
-
-
-                    drawConnectors(canvasCtx, landmarks, [[469, 470], [470, 471]], {color: '#FF3030'});
-
-
-                }
-
-            }
-            canvasCtx.restore();
-        }
-
-        const faceMesh = new FaceMesh({
-            locateFile: (file) => {
-                return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
-            }
-        });
-        faceMesh.setOptions({
-            maxNumFaces: 1,
-            refineLandmarks: true,
-            minDetectionConfidence: 0.5,
-            minTrackingConfidence: 0.5
-        });
-        faceMesh.onResults(onResults);
-
-        const camera = new Camera(videoElement, {
-            onFrame: async () => {
-                await faceMesh.send({image: videoElement});
-            },
-            width: videoElement.offsetWidth,
-            height: videoElement.offsetHeight
-        });
-        camera.start();
-
 
     })
 })
