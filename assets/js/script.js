224f4a510a0039936a4c7f5d70223a78d5cf86ec
mohammadmizan <takbir.jcd@gmail.com>
2022-05-31 18:35:11 +0600

    for result I hope its work

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,7 @@ const actionBtn = document.querySelector('.try');
 actionBtn.addEventListener('click', () => {
     imgContent.classList.add('loadingContent');
     imgContent.innerHTML = `
-     <iframe src="./canvas.html" style="width: 100%; height: 100%;"></iframe>
- `;
+     <iframe src="./canvas.html" style="width: 100%; height: 100%;"></iframe>`;
 })
 
 
